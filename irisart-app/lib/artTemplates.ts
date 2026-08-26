faa6c9fd298176aa9184ddf036c95736e62be9da
Amir Figut <amirfigut@gmail.com>
2026-08-26 21:11:14 +0200

    templates

--- irisart-app/lib/artTemplates.ts
+++ irisart-app/lib/artTemplates.ts
@@ -100,8 +100,8 @@ export const ART_TEMPLATES: ArtTemplate[] = [
   colorFamilies: ['any'],
   tintWithIrisColor: true,
   irisHole: {
-    x: 0.168,
-    y: 0.187,
+    x: 0.165,
+    y: 0.185,
     w: 0.684,
     h: 0.634,
     circular: true,
