374f0c9913d4945499b4162e486e027296b9e492
Amir Figut <amirfigut@gmail.com>
2026-08-27 14:16:34 +0200

    templates

--- irisart-app/lib/artTemplates.ts
+++ irisart-app/lib/artTemplates.ts
@@ -149,14 +149,14 @@ export const ART_TEMPLATES: ArtTemplate[] = [
   irisHole: {
     x: 0.2038,
     y: 0.1683,
-    w: 0.3308,
-    h: 0.3644,
+    w: 0.3908,
+    h: 0.4244,
   },
   irisHoles: [
     { x: 0.2038, y: 0.1683, w: 0.3308, h: 0.3644 }, // oben links = 1. Iris
     { x: 0.4705, y: 0.45, w: 0.38, h: 0.35 }, // unten rechts = 2. Iris
   ],
-  irisScale: 1.25,
+  irisScale: 1.2,
   overlayImage: require('@/assets/art-templates/doublegalaxy.png'),
 },
 
