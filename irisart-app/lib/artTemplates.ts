c7a696918b0e2e39b6e505bbe6aa41044d6eda78
Amir Figut <amirfigut@gmail.com>
2026-08-28 16:11:07 +0200

    double galaxy iris scale

--- irisart-app/lib/artTemplates.ts
+++ irisart-app/lib/artTemplates.ts
@@ -147,8 +147,8 @@ export const ART_TEMPLATES: ArtTemplate[] = [
   tintWithIrisColor: true,
   multiColorTint: true,
   irisHole: {
-    x: 0.1415,
-    y: 0.164,
+    x: 0.1412,
+    y: 0.1635,
     w: 0.73,
     h: 0.69,
     circular: true,
