a1e66375a9177f35a449104b689c09d36535c81e
Amir Figut <amirfigut@gmail.com>
2026-08-28 15:52:56 +0200

    halo

--- irisart-app/lib/artTemplates.ts
+++ irisart-app/lib/artTemplates.ts
@@ -147,13 +147,13 @@ export const ART_TEMPLATES: ArtTemplate[] = [
   tintWithIrisColor: true,
   multiColorTint: true,
   irisHole: {
-    x: 0.138,
+    x: 0.14,
     y: 0.167,
     w: 0.7,
     h: 0.66,
     circular: true,
   },
-  irisScale: 1.25,
+  irisScale: 1.275,
   overlayImage: require('@/assets/art-templates/halo.png'),
 },
 
